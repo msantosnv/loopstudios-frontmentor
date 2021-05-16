2c2b3b128b1106ba064466e4738359e8327dc1de
Monica <msantos.nv@gmail.com>
2021-05-16 01:13:56 +0100

    conclusao mobile

---
+++ assets/js/btn.js
@@ -0,0 +1,23 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const topmenu = document.getElementById('top');
+  topmenu.classList.toggle('topmenu');
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+
+const dimensaoAtual = window.innerWidth;
+function trocarImagem() {
+  if (dimensaoAtual <= 768) {
+    const img = document.querySelector('.image');
+    img.setAttribute('src', '../assets/images/mobile/mob-image-interactive.jpg');
+
+  }
+}
+trocarImagem()
